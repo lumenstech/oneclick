cb489c12e945d6260b6389d00f9dd632a99ef183
lumenstech <124342884+lumenstech@users.noreply.github.com>
2026-08-10 11:40:04 -0400

    chore(node): extend local gate to build node binary

--- scripts/check.sh
+++ scripts/check.sh
@@ -13,6 +13,8 @@ go vet ./...
 tmp="$(mktemp -d)"
 trap 'rm -rf "$tmp"' EXIT
 CGO_ENABLED=0 go build -trimpath -o "$tmp/oneclick" ./cmd/oneclick
+CGO_ENABLED=0 go build -trimpath -o "$tmp/oneclick-node" ./cmd/oneclick-node
 GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go build -trimpath -o "$tmp/oneclick.exe" ./cmd/oneclick
 GOOS=darwin GOARCH=arm64 CGO_ENABLED=0 go build -trimpath -o "$tmp/oneclick-darwin" ./cmd/oneclick
 "$tmp/oneclick" version
+"$tmp/oneclick-node" version
